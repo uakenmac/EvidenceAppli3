5b36bef15e1d4f988f77262db22985caa79a6b7f
unaken <tarekenroom@gmail.com>
2020-08-01 22:59:04 +0900

    更新
    更新

--- EvidenceAppli/MainForm.Designer.cs
+++ EvidenceAppli/MainForm.Designer.cs
@@ -345,7 +345,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 404);
+            this.ClientSize = new System.Drawing.Size(494, 403);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.panel1);
